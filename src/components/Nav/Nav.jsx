c8f5751b7f788e9ae2c18b71e06ee65eccf0d7b5
Jacob Larson <hockeybobber@comcast.net>
2025-12-28 19:11:42 -0600

    fixed the nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,15 +11,8 @@ function Nav(props) {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Bowsermon</h2>
+        <h2 className="nav-title">Nail Saloon</h2>
       </Link>
-      {/* If no user is logged in, show these links */}
-      {!user.id && (
-        // If there's no user, show login/registration links
-        <Link className="navLink" to="/login">
-          Login / Register
-        </Link>
-      )}
     </div>
   );
 }
