d0d7daace67a986aeed8b105c4a6ad3354b622d5
Jacob Larson <hockeybobber@comcast.net>
2025-12-30 19:37:22 -0600

    addde nav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,17 @@ import Home from "../Home/Home.jsx";
 import About from "../AboutPage/AboutPage.jsx";
 import SchedulePage from "../SchedulePage/SchedulePage.jsx";
 import ScheduleForm from "../ScheduleForm/ScheduleForm.jsx";
+import Nav from "../Nav/Nav.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">Nail Saloon</h1>
+      {/* <h1 className="App-header">Nail Saloon</h1> */}
 
       <Router>
+      <Nav />
+
         <Route path="/" exact>
           <Home />
         </Route>
