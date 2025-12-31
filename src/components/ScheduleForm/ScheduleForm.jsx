5265d6061adf1941b3ba49f510be75912b38959c
Jacob Larson <hockeybobber@comcast.net>
2025-12-31 17:30:21 -0600

    formatted

--- src/components/ScheduleForm/ScheduleForm.jsx
+++ src/components/ScheduleForm/ScheduleForm.jsx
@@ -1,17 +1,16 @@
-import React, { useState }from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const TodoForm = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <h3>This is the form</h3>
+  return (
+    <div>
+      <h3>This is the form</h3>
 
-            <p>type stuff here</p>
-        </div>
-    );
-}
-
+      <p>type stuff here</p>
+    </div>
+  );
+};
 
 export default TodoForm;
