2c455cdcd608804f55d6f06f907e355f69af26a6
Jacob Larson <hockeybobber@comcast.net>
2025-12-30 19:24:20 -0600

    fixed import error with link

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import ScheduleForm from '../ScheduleForm/ScheduleForm.jsx';
 import SchedulePage from '../SchedulePage/SchedulePage.jsx';
 
