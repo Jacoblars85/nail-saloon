d0c300be81dd7ec7c2030209d8cb2b4600aa5129
Jacob Larson <hockeybobber@comcast.net>
2025-12-31 17:48:52 -0600

    swap code around

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState, forwardRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Nav.css";
-import { useSelector, useDispatch } from "react-redux";
 
 function Nav() {
   const dispatch = useDispatch();
