3734327e0ee672082560c96550205805c5a013be
Jacob Larson <hockeybobber@comcast.net>
2026-01-01 17:32:35 -0600

    removed old code

--- src/components/ScheduleForm/ScheduleForm.jsx
+++ src/components/ScheduleForm/ScheduleForm.jsx
@@ -1,20 +1,3 @@
-// import React, { useState } from "react";
-// import { useDispatch } from "react-redux";
-
-// const  = () => {
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       <h3>This is the form</h3>
-
-//       <p>type stuff here</p>
-//     </div>
-//   );
-// };
-
-// export default TodoForm;
-
 import { useSelector, useDispatch } from 'react-redux'
 import React, { useState } from 'react';
 
