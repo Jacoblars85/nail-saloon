bc983f84460cee2349f77350f86d2d5a3657c09d
Jacob Larson <hockeybobber@comcast.net>
2026-01-02 20:13:19 -0600

    fixed up names and added the schedelue form inside

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ScheduleForm from "../ScheduleForm/ScheduleForm.jsx";
 
-function TodoList() {
+
+function SchedulePage() {
   const dispatch = useDispatch();
 
   useEffect(() => {}, []);
 
   return (
-    <div>
-      <h3>This is the todo list</h3>
+    <div className="SchedulePage">
+      <h2>Schedule an Appointment Here!</h2>
+
+      <ScheduleForm />
     </div>
   );
 }
 
-export default TodoList;
+export default SchedulePage;
