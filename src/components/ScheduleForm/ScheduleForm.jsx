2b5f60277b8502faf2a970bf7de2d9c9d5d2357e
Jacob Larson <hockeybobber@comcast.net>
2026-01-06 19:29:59 -0600

    rremeoved the old component

--- src/components/ScheduleForm/ScheduleForm.jsx
+++ src/components/ScheduleForm/ScheduleForm.jsx
@@ -1,17 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import React, { useState } from "react";
-
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
-// export default function FirstComponent() {
-//   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <DatePicker />
-//     </LocalizationProvider>
-//   );
-// }
 
 function ScheduleForm() {
   const dispatch = useDispatch();
