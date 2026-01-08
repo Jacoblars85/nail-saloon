093524b52c18a596bf351a65b548830a39e1a600
Jacob Larson <hockeybobber@comcast.net>
2026-01-08 13:01:34 -0600

    deleted the time avalible

--- database.sql
+++ database.sql
@@ -5,34 +5,7 @@ CREATE TABLE "appointments" (
   "start_time" TIMESTAMP NOT NULL
 );
 
-CREATE TABLE "time_available" (
-    "id" SERIAL PRIMARY KEY,
-    "time" VARCHAR (100) NOT NULL,
-    "taken" BOOLEAN DEFAULT FALSE
-);
-
 INSERT INTO "appointments" 
 ("name", "phone", "start_time")
 VALUES 
 ('Jane Doe', '555-1234', '2026-01-08 10:00');
-
-  INSERT INTO "time_available" 
-	("time", "taken")
-	VALUES 
-  ('10:00 AM', FALSE),
-  ('10:30 AM', FALSE),
-  ('11:00 AM', FALSE),
-  ('11:30 AM', FALSE),
-  ('12:00', FALSE),
-  ('12:30 PM', FALSE),
-  ('1:00 PM', FALSE),
-  ('1:30 PM', FALSE),
-  ('2:00 PM', FALSE),
-  ('2:30 PM', FALSE),
-  ('3:00 PM', FALSE),
-  ('3:30 PM', FALSE),
-  ('4:00 PM', FALSE),
-  ('4:30 PM', FALSE),
-  ('5:00 PM', FALSE),
-  ('5:30 PM', FALSE),
-  ('6:00 PM', FALSE);
