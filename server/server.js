6881582c72452679428847c234b5fcde26d425b0
Jacob Larson <hockeybobber@comcast.net>
2026-01-08 12:48:40 -0600

    changed the router name

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const todoRouter = require('./routes/todo.router');
+const scheduleRouter = require('./routes/schedule.router');
 const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
 app.use(express.static('build'));
 
-app.use('/api/todo', todoRouter);
+app.use('/api/schedule', scheduleRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
