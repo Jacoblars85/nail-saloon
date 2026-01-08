a8f9ed81cba789990b0b423cb7d5a3a6405b63aa
Jacob Larson <hockeybobber@comcast.net>
2026-01-08 13:10:07 -0600

    added getting all open appointments

--- server/routes/schedule.router.js
+++ server/routes/schedule.router.js
@@ -3,8 +3,31 @@ const pool = require('../modules/pool');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/open/appointments", (req, res) => {
+  // console.log('im in open appointments route');
 
+  const query = `
+SELECT *
+FROM generate_series(
+  '2026-01-08 10:00'::timestamp,
+  '2026-01-08 18:00'::timestamp,
+  interval '30 minutes'
+) AS slot
+WHERE slot NOT IN (
+  SELECT "start_time" FROM "appointments"
+);
+
+    `;
+
+  pool
+    .query(query)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all open appointments", err);
+      res.sendStatus(500);
+    });
 });
 
 router.get('/', (req, res) => {
