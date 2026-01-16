05b4f504366fd9d5b58180030452f39648635a46
Jacob Larson <hockeybobber@comcast.net>
2026-01-15 18:49:51 -0600

    added drop table for appointment table

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS appointments;
+
 CREATE TABLE "appointments" (
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(100) NOT NULL,
@@ -11,4 +13,4 @@ ON "appointments" ("start_time");
 INSERT INTO "appointments" 
 ("name", "phone", "start_time")
 VALUES 
-('Jane Doe', '555-1234', '2026-01-08 10:00');
+('Jane Doe', '555-1234', '2026-01-30 15:00');
