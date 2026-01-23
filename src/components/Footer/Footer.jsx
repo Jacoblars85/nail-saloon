78b44b6a041da5b7397760fd86d6e3b8c40fe438
Jacob Larson <hockeybobber@comcast.net>
2026-01-23 16:39:06 -0600

    added another section in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,9 +7,15 @@ function Footer() {
 
   return (
     <footer className="Footer">
+        <div>
       <h2>Contact Information</h2>
       <p>Email: </p>
       <p>phone Number: </p>
+      </div>
+      <div><h2>Hours</h2>
+      <p>weekdays: 10-6</p>
+      <p>weekends: 10-6</p>
+      <p>Holidays: Closed</p></div>
     </footer>
   );
 }
