eb220ecf1de8eed2c863277a81c0ca942d4d7048
Jacob Larson <hockeybobber@comcast.net>
2026-01-23 16:39:21 -0600

    formatted

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,17 @@ function Footer() {
 
   return (
     <footer className="Footer">
-        <div>
-      <h2>Contact Information</h2>
-      <p>Email: </p>
-      <p>phone Number: </p>
+      <div>
+        <h2>Contact Information</h2>
+        <p>Email: </p>
+        <p>phone Number: </p>
       </div>
-      <div><h2>Hours</h2>
-      <p>weekdays: 10-6</p>
-      <p>weekends: 10-6</p>
-      <p>Holidays: Closed</p></div>
+      <div>
+        <h2>Hours</h2>
+        <p>weekdays: 10-6</p>
+        <p>weekends: 10-6</p>
+        <p>Holidays: Closed</p>
+      </div>
     </footer>
   );
 }
