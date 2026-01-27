bfc84aa89a7741d04d49a76a3c19a432e42b483b
Jacob Larson <hockeybobber@comcast.net>
2026-01-27 16:10:28 -0600

    added important links div

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,6 +11,13 @@ function Footer() {
         <p>Number: 123-456-7890</p>
       </div>
       <div>
+        <h3>Important Links</h3>
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Schedule Now</p>
+        <p></p>
+      </div>
+      <div>
         <h3>Hours</h3>
         <p>weekdays: 10-6</p>
         <p>weekends: 10-6</p>
