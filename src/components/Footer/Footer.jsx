7bd6434ad503879dc4f9e11059fe561afdcbd9a0
Jacob Larson <hockeybobber@comcast.net>
2026-01-27 16:17:32 -0600

    added links to the p tags

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   useEffect(() => {}, []);
@@ -12,10 +13,15 @@ function Footer() {
       </div>
       <div>
         <h3>Important Links</h3>
+        <Link to="/">
         <p>Home</p>
+        </Link>
+        <Link to="/about">
         <p>About Us</p>
+        </Link>
+         <Link to="/schedule">
         <p>Schedule Now</p>
-        <p></p>
+        </Link>
       </div>
       <div>
         <h3>Hours</h3>
