ddbec98452c8e640882427afb2ec5a25d24dcb16
Jacob Larson <hockeybobber@comcast.net>
2026-01-29 13:27:23 -0600

    removed unused imports

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import ScheduleForm from "../ScheduleForm/ScheduleForm.jsx";
 import Button from "@mui/material/Button";
 
 function SchedulePage() {
