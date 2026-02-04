a3fbacbd6ee36e4e377befa9462def6036f33dca
Jacob Larson <hockeybobber@comcast.net>
2026-02-04 16:15:44 -0600

    formatted

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -28,8 +28,8 @@ function Admin() {
   }, []);
 
   const getTodaysAppointments = (date) => {
-    console.log('date', date);
-    
+    console.log("date", date);
+
     axios({
       method: "GET",
       url: `/api/schedule/todays/appointments/${date}`,
