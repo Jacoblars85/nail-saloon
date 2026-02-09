1d5334ac446f3c99c2f93bf0f128c76a4c954f01
Jacob Larson <hockeybobber@comcast.net>
2026-02-09 10:29:41 -0600

    commented out the old use effect

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -28,10 +28,10 @@ function Admin() {
   const [openForm, setOpenForm] = useState(false);
   const [openDelete, setOpenDelete] = useState(false);
 
-  useEffect(() => {
-    getBookedAppointments();
-    getTodaysAppointments(dayjs());
-  }, []);
+  // useEffect(() => {
+  //   getBookedAppointments();
+  //   getTodaysAppointments(dayjs());
+  // }, []);
 
   useEffect(() => {
     if (dateSelect === "today") {
